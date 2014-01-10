c83f891c3e6afd5ee398dd70f2e88bb3974ce565
PikachuEXE <pikachuexe@gmail.com>
2014-01-10 17:14:22 +0800

    * Use correct lookup_context

--- test/caching/template_finder_test.rb
+++ test/caching/template_finder_test.rb
@@ -7,56 +7,56 @@ class TemplateFinderTest < MiniTest::Spec
   before :each do
     # ActionController::Base.cache_store.clear
     # ActionController::Base.perform_caching = true
-    @cell = cell(:bassist)
-    @lookup_context = @cell.lookup_context
+    @bassist = cell(:bassist)
+    @bad_guitarist = cell(:bad_guitarist)
     @class = Cell::Caching::TemplateFinder
   end
 
   describe '.find' do
     it "returns template instance when template is present" do
-      template = @lookup_context.find('bassist/promote', [], false)
+      template = @bassist.lookup_context.find('bassist/promote', [], false)
       assert template.formats.include?(:html), "prepared template should have correct format but with #{template.formats}"
 
-      assert_equal @class.find('promote', @cell).source, template.source
+      assert_equal @class.find('promote', @bassist).source, template.source
     end
 
     it "returns digest for the template with format html by default" do
-      template = @lookup_context.find('bassist/play.html', [], false)
+      template = @bassist.lookup_context.find('bassist/play.html', [], false)
       assert template.formats.include?(:html), "prepared template should have correct format but with #{template.formats}"
 
-      assert_equal @class.find('play', @cell).source, template.source
+      assert_equal @class.find('play', @bassist).source, template.source
     end
 
     it "returns digest for the template with correct format" do
-      template = @lookup_context.find('bassist/play.js', [], false)
+      template = @bassist.lookup_context.find('bassist/play.js', [], false)
       assert template.formats.include?(:js), "prepared template should have correct format but with #{template.formats}"
 
-      assert_equal @class.find('play', @cell, format: :js).source, template.source
+      assert_equal @class.find('play', @bassist, format: :js).source, template.source
     end
 
     it "returns digest for the template with locale" do
-      template = @lookup_context.find('bassist/yell.en.html', [], false)
+      template = @bassist.lookup_context.find('bassist/yell.en.html', [], false)
       assert template.formats.include?(:html), "prepared template should have correct format but with #{template.formats}"
 
-      assert_equal @class.find('yell', @cell, format: :html).source, template.source
+      assert_equal @class.find('yell', @bassist, format: :html).source, template.source
     end
 
     it "returns digest for super class's template from inherited cell" do
-      template = @lookup_context.find('bassist/promote', [], false)
+      template = @bad_guitarist.lookup_context.find('bassist/promote', [], false)
       assert template.formats.include?(:html), "prepared template should have correct format but with #{template.formats}"
 
-      assert_equal @class.find('promote', cell(:bad_guitarist), format: :html).source, template.source
+      assert_equal @class.find('promote', @bad_guitarist, format: :html).source, template.source
     end
 
     it "returns digest for its own template from inherited cell if it has" do
-      template = @lookup_context.find('bad_guitarist/_dii', [], false)
+      template = @bad_guitarist.lookup_context.find('bad_guitarist/_dii', [], false)
       assert template.formats.include?(:html), "prepared template should have correct format but with #{template.formats}"
 
-      assert_equal @class.find('_dii', cell(:bad_guitarist), format: :html).source, template.source
+      assert_equal @class.find('_dii', @bad_guitarist, format: :html).source, template.source
     end
 
     it "returns nil when template missing" do
-      assert_nil @class.find('groove', @cell)
+      assert_nil @class.find('groove', @bassist)
     end
   end
 end
