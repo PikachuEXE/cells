499423e2432ca55833120f2ed0f6d0d54fb9e562
PikachuEXE <pikachuexe@gmail.com>
2014-01-10 17:34:34 +0800

    * Make TemplateDigester use TemplateFinder (it was WIP anyway)

--- lib/cell/caching/template_digestor.rb
+++ lib/cell/caching/template_digestor.rb
@@ -10,48 +10,40 @@ module Cell
 
       cattr_accessor(:logger, instance_reader: true)
 
-      def self.digest(name, format, finder, options = {})
-        cache_key = [ "cell_template_digestor", cache_prefix, name, format ].compact.join("/")
-        cache_store.fetch(cache_key) do
-          cache_store.write(cache_key, nil) # Prevent re-entry
-          new(name, format, finder, options).digest
-        end
+      def self.digest(name, cell, options = {})
+        new(name, cell, options).digest
       end
 
-      attr_reader :name, :format, :finder, :options
+      attr_reader :name, :cell, :options
 
-      def initialize(name, format, finder, options = {})
-        @name, @format, @finder, @options = name, format, finder, options
+      def initialize(name, cell, options = {})
+        @name, @cell, @options = name, cell, options
       end
 
       def digest
-        Digest::MD5.hexdigest(source).tap do |digest|
-          logger.try :debug, "Cache digest for #{name}.#{format}: #{digest}"
+        format = options[:format]
+        cache_key = [ "cell_template_digestor", cache_prefix, name, format ].compact.join("/")
+
+        cache_store.fetch(cache_key) do
+          cache_store.write(cache_key, nil) # Prevent re-entry
+          begin
+            Digest::MD5.hexdigest(source).tap do |digest|
+              logger.try :debug, "Cache digest for #{name}.#{format}: #{digest}"
+            end
+          rescue ActionView::MissingTemplate
+            logger.try :error, "Couldn't find template for digesting: #{name}.#{format}"
+            ''
+          end
         end
-      rescue ActionView::MissingTemplate
-        logger.try :error, "Couldn't find template for digesting: #{name}.#{format}"
-        ''
       end
 
       private
-        def name_for_look_up
-          "#{logical_name}.#{format}"
-        end
-
-        def logical_name
-          name.gsub(%r|/_|, "/")
-        end
-
-        def partial?
-          options[:partial] || name.include?("/_")
-        end
-
         def source
           template.source
         end
 
         def template
-          @template ||= finder.find(name_for_look_up, [], partial?)
+          @template ||= TemplateFinder.find(name, cell, options)
         end
     end
   end
--- test/caching/template_digestor_test.rb
+++ test/caching/template_digestor_test.rb
@@ -7,8 +7,8 @@ class TemplateDigestorTest < MiniTest::Spec
   before :each do
     # ActionController::Base.cache_store.clear
     # ActionController::Base.perform_caching = true
-    @cell = cell(:bassist)
-    @lookup_context = @cell.lookup_context
+    @bassist = cell(:bassist)
+    @bad_guitarist = cell(:bad_guitarist)
     @class = Cell::Caching::TemplateDigestor
   end
 
@@ -17,29 +17,55 @@ class TemplateDigestorTest < MiniTest::Spec
   end
 
   describe '.digest' do
-    it "returns digest when template is present" do
-      template = @lookup_context.find('bassist/play.html', [], false)
-      digest = Digest::MD5.hexdigest(template.source)
+    it "returns template instance when template is present" do
+      template = @bassist.lookup_context.find('bassist/promote', [], false)
+      assert template.formats.include?(:html), "prepared template should have correct format but with #{template.formats}"
+      begin
+        @class.digest('promote', @bassist)
+      rescue => e
+        puts e.backtrace
+      end
 
-      assert_equal @class.digest('bassist/play', :html, @lookup_context), digest
+      # assert_equal @class.digest('promote', @bassist), Digest::MD5.hexdigest(template.source)
+    end
+
+    it "returns digest for the template with format html by default" do
+      template = @bassist.lookup_context.find('bassist/play.html', [], false)
+      assert template.formats.include?(:html), "prepared template should have correct format but with #{template.formats}"
+
+      assert_equal @class.digest('play', @bassist), Digest::MD5.hexdigest(template.source)
     end
 
     it "returns digest for the template with correct format" do
-      template = @lookup_context.find('bassist/play.js', [], false)
-      digest = Digest::MD5.hexdigest(template.source)
+      template = @bassist.lookup_context.find('bassist/play.js', [], false)
+      assert template.formats.include?(:js), "prepared template should have correct format but with #{template.formats}"
 
-      assert_equal @class.digest('bassist/play', :js, @lookup_context), digest
+      assert_equal @class.digest('play', @bassist, format: :js), Digest::MD5.hexdigest(template.source)
     end
 
-    it "returns differnet digest for a template with same name but differnet format" do
-      template = @lookup_context.find('bassist/play.js', [], false)
-      digest = Digest::MD5.hexdigest(template.source)
+    it "returns digest for the template with locale" do
+      template = @bassist.lookup_context.find('bassist/yell.en.html', [], false)
+      assert template.formats.include?(:html), "prepared template should have correct format but with #{template.formats}"
 
-      assert @class.digest('bassist/play', :html, @lookup_context) != digest
+      assert_equal @class.digest('yell', @bassist, format: :html), Digest::MD5.hexdigest(template.source)
     end
 
-    it "returns empty string when template missing" do
-      assert_equal @class.digest('bassist/groove', :html, @lookup_context), ""
+    it "returns digest for super class's template from inherited cell" do
+      template = @bad_guitarist.lookup_context.find('bassist/promote', [], false)
+      assert template.formats.include?(:html), "prepared template should have correct format but with #{template.formats}"
+
+      assert_equal @class.digest('promote', @bad_guitarist, format: :html), Digest::MD5.hexdigest(template.source)
+    end
+
+    it "returns digest for its own template from inherited cell if it has" do
+      template = @bad_guitarist.lookup_context.find('bad_guitarist/_dii', [], false)
+      assert template.formats.include?(:html), "prepared template should have correct format but with #{template.formats}"
+
+      assert_equal @class.digest('_dii', @bad_guitarist, format: :html), Digest::MD5.hexdigest(template.source)
+    end
+
+    it "returns nil when template missing" do
+      assert_equal @class.digest('groove', @bassist), ''
     end
   end
 end
